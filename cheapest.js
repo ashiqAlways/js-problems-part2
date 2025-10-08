cb71408bec3fc454b0224a77732ec1f637be4a60
Ashik <ashikkhan757@gmail.coml>
2025-10-08 22:59:24 +0600

    added some file

--- cheapest.js
+++ cheapest.js
@@ -5,15 +5,39 @@ const phones = [
     {name: 'Iphone', price: 100000, camera: '12mp', color: 'black' },
     {name: 'Walton', price: 31000, camera: '12mp', color: 'black' },
     {name: 'HTC', price: 27000, camera: '12mp', color: 'black' },
-]
+];
 
 
 function getCheapestPhone(phones){
+    let min = phones[0];
     for(const phone of phones){
-        console.log(phone)
+        
+        if(phone.price < min.price){
+            min = phone; 
+        }
     }
+    return min;
 }
 
 
 const cheep = getCheapestPhone(phones);
 console.log('Cheapest phone is:', cheep);
+
+
+
+
+// home work: Expensive price;
+
+function getExpensivePhone(phones){
+    let max = phones[0];
+    for(const phone of phones){
+        if(phone.price > max.price){
+            max = phone;
+        }
+    }
+    return max;
+}
+
+
+const expensive = getExpensivePhone(phones);
+console.log('Expensive phone is:', expensive);
