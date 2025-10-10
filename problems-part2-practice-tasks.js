4af7ad3f7a1f1e8c3b00631cdef329a51ffea63e
Ashik <ashikkhan757@gmail.coml>
2025-10-10 17:55:03 +0600

    update some code

--- problems-part2-practice-tasks.js
+++ problems-part2-practice-tasks.js
@@ -43,7 +43,8 @@ for(const name of nameList){
 
 
 
-//---------- Task 03 --------------
+//---------- Task 03 -----------------
+// Your task is to calculate the total budget required to buy electronics:
 
 const products = [
     {name: 'laptop', price: 35000},
@@ -63,7 +64,9 @@ const total = getTotalBudget(products);
 // console.log('Total budget is:', total);
 
 
-//---------- Task 04 --------------
+//---------- Task 04 --------------------
+// Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required:
+
 
     const phones = [
         { model: "PhoneA", brand: "Iphone", price: 95000 },
@@ -86,5 +89,34 @@ const total = getTotalBudget(products);
         return avg;  
     }
 
-    const averagePrice =findAveragePhonePrice(phones)
-    console.log(averagePrice);
+    const averagePrice =findAveragePhonePrice(phones);
+    // console.log(averagePrice);
+
+
+
+//---------- Task 05 --------------------
+// For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
+
+ const employees = [
+            { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+            { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+            { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+            { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+        ];
+
+    
+        function getTotalSalary(employees){
+            let total = 0;
+            for(const emp of employees){
+                const increase = emp.experience * emp.increment;
+                const currentSalary = emp.starting + increase;
+                total = total + currentSalary;
+            }
+            return total;
+        }
+
+
+        const totalSalary = getTotalSalary(employees)
+        console.log(totalSalary);
+
+
